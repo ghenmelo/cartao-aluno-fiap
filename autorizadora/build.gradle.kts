4eb42a43777490caa048a1fc0eab131bd63fc97a
Guilherme Melo <gui.henmelo@gmail.com>
2022-10-02 22:05:44 -0300

    commit finalizando e doc

--- autorizadora/build.gradle.kts
+++ autorizadora/build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.4")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("junit:junit:4.13.2")
 }
